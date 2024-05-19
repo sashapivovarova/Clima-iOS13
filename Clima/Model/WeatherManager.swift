947c0523604cf61951c9affe8c855a7e6d4656ab
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-05-19 21:43:59 +0900

    closure の理解

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -19,19 +19,16 @@ class WeatherManager {
     func performRequest(urlString: String) {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                }
+            }
             task.resume()
-        }
-    }
-    
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
         }
     }
 }

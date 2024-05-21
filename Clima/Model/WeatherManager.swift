50475d94bddf11d02a2a3bacde41536f50490765
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-05-21 21:32:55 +0900

    Basic getConditionName の追加

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -35,9 +35,31 @@ class WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            
+            let id = decodedData.weather[0].id
+            getConditionName(weatherId: id)
         } catch {
             print(error)
+        }
+    }
+    
+    func getConditionName(weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
         }
     }
 }
